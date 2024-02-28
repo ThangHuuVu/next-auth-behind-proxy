ecde4ae9ee302e70e4d170011d7f65e6866804f4
Thang Vu <hi@thvu.dev>
2024-02-28 22:49:17 +0700

    add asset Prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/authjs" : undefined,
   basePath: "/authjs",
 };
