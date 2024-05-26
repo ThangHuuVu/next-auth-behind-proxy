95f650fd7466ce9849301900f5aa28a4162fa33d
Thang Vu <hi@thvu.dev>
2024-05-26 16:26:37 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,25 @@
-import { handlers } from "auth"
-export const { GET, POST } = handlers
+import { handlers } from "auth";
+import { NextRequest } from "next/server";
+
+const reqWithBasePath = (req: NextRequest, basePath = "/authjs") => {
+  const baseUrl = new URL(
+    (req.headers.get("x-forwarded-proto") ?? "https") +
+      "://" +
+      (req.headers.get("x-forwarded-host") ?? req.nextUrl.host)
+  );
+  const urlWithBasePath = new URL(
+    `${basePath}${req.nextUrl.pathname}${req.nextUrl.search}`,
+    baseUrl
+  );
+  return new NextRequest(urlWithBasePath.toString(), req);
+};
+
+export const GET = (req: NextRequest) => {
+  const nextReq = reqWithBasePath(req);
+  return handlers.GET(nextReq);
+};
+
+export const POST = (req: NextRequest) => {
+  const nextReq = reqWithBasePath(req);
+  return handlers.POST(nextReq);
+};
